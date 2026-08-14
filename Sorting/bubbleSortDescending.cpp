b013359be975e9b02b9a32f28bfd0cef43eea986
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-14 19:59:17 +0530

    Add bubble sort implementation for descending order
    Implement bubble sort algorithm to sort an array in descending order.

---
+++ Sorting/bubbleSortDescending.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    vector<int>arr  = {8,1,-7,4,-1,11,-5,7,2,9};
+    int n = arr.size();
+    //Bubble Sort in descending (reverse) order
+    for(int i=0;i<n-1;i++){
+        int swaps = 0;
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]<arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swaps++;
+            }
+        }
+        if(swaps ==0 )break;
+    }
+    for(auto ele: arr){
+        cout<<ele<<" ";
+    }
+}
